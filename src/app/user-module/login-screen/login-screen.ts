fea6f8a9d1397673280dee5bec5db83462e99dd1
marcelaamaro <marcelaamaromel@gmail.com>
2025-09-04 21:54:42 -0300

    Validacao de login

--- src/app/user-module/login-screen/login-screen.ts
+++ src/app/user-module/login-screen/login-screen.ts
@@ -8,25 +8,54 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angula
   styleUrl: './login-screen.css'
 })
 export class LoginScreen {
- loginForm: FormGroup;
 
-    constructor(private fb: FormBuilder) {
-      //quando a tela iniciar.
+  loginForm: FormGroup;
 
-      //inicia o formulario.
-      //cria o campo obrigatorio de email.
-      //cria o campo obrigatorio de senha.
-      this.loginForm = this.fb.group({
-        email: ["", [Validators.required]],
-        password: ["", [Validators.required]]   });
+  constructor(private fb: FormBuilder) {
+    //quando a tela iniciar.
+
+    //inicia o formulario.
+    //cria o campo obrigatorio de email.
+    //cria o campo obrigatorio de senha.
+    this.loginForm = this.fb.group({
+      email: ["", [Validators.required]],
+      password: ["", [Validators.required]]
+    });
 
 
+  }
+
+  async onloginClick() {
+    alert("botao de login clicado.");
+
+    console.log("email", this.loginForm.value.email);
+    console.log("Password", this.loginForm.value.password);
+
+    if (this.loginForm.value.email == ""){
+    alert("Preencha o e-mail.");
+    return;
     }
 
-    onloginClick() {
-      alert("botao de login clicado.");
+    let response = await fetch("https://senai-gpt-api.azurewebsites.net/login", {
+      method: "POST", //enviar informacao
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify({
+        email: this.loginForm.value.email,
+        password: this.loginForm.value.password
+      })
+    });
+    console.log("STATUS CODE", response.status);
 
-      console.log("email", this.loginForm.value.email);
-      console.log("Password", this.loginForm.value.password);
-    }
+
+if (response.status <=299) {
+//Bloco positivo
+alert ("Deu tudo certo");
+ } else {
+//Bloco do falso
+alert ("deu errado");
+
+}
+}
   }
